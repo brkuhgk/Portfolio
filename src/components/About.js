15bbd60b9fe294f9188d9b8638b47036e2aca9d8
karthik <43676649+brkuhgk@users.noreply.github.com>
2021-06-23 13:55:50 +0530

    typo updated quote

--- src/components/About.js
+++ src/components/About.js
@@ -12,9 +12,9 @@ export default function About() {
             apps.
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+            Set no bounderies to learn, Most of my I spend wrting code not because of fun always,
+            It also teaches discipline and planing.
+   
           </p>
           <div className="flex justify-center">
             <a
