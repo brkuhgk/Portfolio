38aa561a1dfa2a102526bd7716f0c3d4afa6922b
karthik <43676649+brkuhgk@users.noreply.github.com>
2021-06-23 14:08:41 +0530

    typo

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,9 +13,8 @@ export default function Skills() {
             Skills &amp; Technologies
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-            possimus est.
+           I drive into most of the technologies but some of them which i focused most was these.
+           i do practice daily to master more skills.
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
